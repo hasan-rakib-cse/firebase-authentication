054ad5e088f2dccb32648393e427fb7d62a34216
MD. RAKIB HASAN <hasanrakibcse@gmail.com>
2023-10-15 18:32:43 +0600

    authentication with google done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,78 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
+import firebaseConfig from './firebase.config';
+import { getAuth, signInWithPopup, GoogleAuthProvider, signOut } from "firebase/auth";
+
+
+firebase.initializeApp(firebaseConfig)
 
 function App() {
+
+  const [user, setUser] = useState({
+    isSignedIn: false,
+    name: '',
+    email: '',
+    photo: ''
+  })
+
+  const auth = getAuth();
+  const provider = new GoogleAuthProvider();
+
+  const handleSignIn = () => {
+    signInWithPopup(auth, provider)
+    .then((result) => {
+
+      // This gives you a Google Access Token. You can use it to access the Google API.
+      // const credential = GoogleAuthProvider.credentialFromResult(result);
+      // const token = credential.accessToken;
+      const {displayName, photoURL, email} = result.user;
+      const signedInUser = {
+        isSignedIn: true,
+        name: displayName,
+        email: email,
+        photo: photoURL
+      }
+      setUser(signedInUser);
+
+    }).catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      const email = error.customData.email;
+      const credential = GoogleAuthProvider.credentialFromError(error);
+
+      console.log(errorCode, errorMessage, email, credential);
+    });
+  }
+
+  const handleSignOut = () => {
+    signOut(auth).then((res) => {
+      const signedOutUser = {
+        isSignedIn: false,
+        name: '',
+        email: '',
+        photo: ''
+      }
+      setUser(signedOutUser);
+    }).catch((error) => {
+      
+    });
+  }
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      { user.isSignedIn ? <button onClick={handleSignOut}>Sign Out</button> : <button onClick={handleSignIn}>Sign In</button> }
+
+      {
+        user.isSignedIn && <div>
+          <img src={user.photo} alt={user.name} />
+          <p>Welcome, <b>{user.name}</b></p>
+          <p>Email: {user.email}</p>
+        </div>
+      }
     </div>
   );
 }
---
+++ src/firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDr_5H1zFFDUr_SZLSUd0PFJjHBq0FSMcA",
+    authDomain: "ema-john-firebase-au.firebaseapp.com",
+    projectId: "ema-john-firebase-au",
+    storageBucket: "ema-john-firebase-au.appspot.com",
+    messagingSenderId: "727465130392",
+    appId: "1:727465130392:web:cdca80f8d1e6062f33d3ed"
+  };
+
+  export default firebaseConfig;
